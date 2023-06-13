c464824d9ac13d419051f9a99e6e307ecd57196a
Wasif Khan <wasif.k1112@gmail.com>
2023-06-13 17:45:22 -0400

    Delete exercises/dictionaries_and_arrays directory

--- exercises/dictionaries_and_arrays/1_sum_tuples/solution/test_solution.py
+++
@@ -1,6 +0,0 @@
-def test_solution(monkeypatch):
-    from solution import sum_tuples
-
-    assert sum_tuples([(1,2), (3,4)]) == [3, 7]
-    assert sum_tuples([(-1,-2)]) == [-3]
-    assert sum_tuples([]) == []
--- exercises/dictionaries_and_arrays/3_prime_factors/solution/test_solution.py
+++
@@ -1,8 +0,0 @@
-def test_solution():
-    from solution import prime_factors
-
-    assert prime_factors(1) == [1]
-    assert prime_factors(13) == [1, 13]
-    assert prime_factors(4) == [1, 2]
-    assert prime_factors(10) == [1, 2, 5]
-    assert prime_factors(12) == [1, 2, 3]
