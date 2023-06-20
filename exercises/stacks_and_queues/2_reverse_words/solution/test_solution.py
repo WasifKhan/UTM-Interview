e0fe038dac061f5e9915d55dbf3fc580da9a0cdc
Wasif Khan <wasif.k1112@gmail.com>
2023-06-20 19:28:53 -0400

    Delete exercises/stacks_and_queues/2_reverse_words directory

--- exercises/stacks_and_queues/2_reverse_words/solution/test_solution.py
+++
@@ -1,7 +0,0 @@
-def test_solution():
-    from solution import reverse_words
-
-    assert reverse_words("hi there") == "there hi"
-    assert reverse_words("hello") == "hello"
-    assert reverse_words("") == ""
-    assert reverse_words("don't forget to be awesome") == "awesome be to forget don't"
